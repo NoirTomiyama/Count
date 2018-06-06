70f7cbe04e06732a28892e1033141b241f4f39c6
RyotaTomiyama <lit.karu.noir@gmail.com>
2018-06-06 11:59:07 +0900

    CheckWork用コメントアウト処理

--- app/src/main/java/com/lifeistech/android/count/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/count/MainActivity.java
@@ -7,6 +7,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    //TODO branchきって、共有する．
+
+    Step1:①~⑥の従って，コードを書いてみよう！(Step1はググルの禁止です！ググるときはメンターに宣言してね！)
+    Step2:Step1が終わって時間があまったら、以下のことをしよう！ここからはググってもOK!!!
+        Ex1: カウントのテキストの色を変更する
+        Ex2: ボタンの背景の色を変更する
+        Ex3: 全体の背景の色を変更する
+    Step3:Step2が終わって時間が余ったら、以下のことをしよう！ここからはググってもOK!!!
+        Super1: 偶数のときだけ、カウントのテキストの色が変わる
+        Super2: 3の倍数のときだけ、ボタンの背景の色が変わる
+        Super3: 5の倍数のときだけ、全体の背景の色が変わる
+
+    */
+
+    // ① 変数の宣言
+
     TextView textView;
     int number;
 
@@ -15,19 +32,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ② xmlファイルのidとの紐づけ
+
         textView = (TextView)findViewById(R.id.textView);
 
+        // ③ 変数の初期化
         number = 0;
-        textView.setText("0");
     }
 
+
+    // ④ plusメソッドの中身を実装
     public void plus(View v){
         number = number + 1;
         textView.setText(String.valueOf(number));
     }
 
+    // ⑤ minusメソッドの中身を実装
     public void minus(View v){
         number = number - 1;
+        textView.setText(String.valueOf(number));
+    }
+
+    // ⑥clearメソッドの中身を実装
+    public void clear(View v){
+        number = 0;
         textView.setText(String.valueOf(number));
     }
 }
